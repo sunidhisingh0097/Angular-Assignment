d5d86c56c951c8cf26ef9e0856441a657d541b87
Sunidhi Singh <sunidhi.singh@nagarro.com>
2020-04-22 15:11:18 +0530

    Assignment Completed

--- src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
+++ src/app/05_assignment/02_assignment2/containers/assignment2.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
+import { Todo } from 'src/app/03_pipes/models/todo.interface';
 
 @Component({
   selector: 'app-assignment2',
@@ -6,5 +7,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./assignment2.component.css']
 })
 export class Assignment2Component {
+
+  selectedRow: Todo[]
+  
+  @Input()
+    todos: Todo[] 
+
+    setClickedRow(todo: Todo[]) {
+    this.selectedRow = todo
+    console.log(todo)
+  }
 
 }
